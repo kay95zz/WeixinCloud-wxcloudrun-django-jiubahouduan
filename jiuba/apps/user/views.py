74ea3168a7a47e60f82e066aebb63426abf7ecfe
kay95zz <908873446@qq.com>
2025-11-09 15:46:16 +0800

    update user views bug

--- jiuba/apps/user/views.py
+++ jiuba/apps/user/views.py
@@ -1,4 +1,5 @@
 # user/views.py
+from django.conf import settings
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
